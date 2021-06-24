0ac8fe315f9847d81b2b0b297c18e21fd0b22a6b
lmeinen <lmeinen@student.ethz.ch>
2021-06-24 15:06:43 +0200

    Implemented JWT validator

--- ch-covidcertificate-backend-delivery/ch-covidcertificate-backend-delivery-ws/src/main/java/ch/admin/bag/covidcertificate/backend/delivery/ws/security/DeliveryJWTValidator.java
+++ ch-covidcertificate-backend-delivery/ch-covidcertificate-backend-delivery-ws/src/main/java/ch/admin/bag/covidcertificate/backend/delivery/ws/security/DeliveryJWTValidator.java
@@ -1,5 +1,7 @@
 package ch.admin.bag.covidcertificate.backend.delivery.ws.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
@@ -8,10 +10,27 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 public class DeliveryJWTValidator implements OAuth2TokenValidator<Jwt> {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public OAuth2TokenValidatorResult validate(Jwt token) {
-        // TODO
+        final var containsClaim = token.containsClaim("resource_access");
+        if (Boolean.TRUE.equals(containsClaim)) {
+            final var claimMap = token.getClaimAsString("resource_access");
+            try {
+                if (objectMapper
+                        .readTree(claimMap)
+                        .get("ch-covidcertificate-backend-delivery-ws")
+                        .get("roles")
+                        .asText()
+                        .contains("certificatecreator")) {
+                    return OAuth2TokenValidatorResult.failure(
+                            new OAuth2Error(OAuth2ErrorCodes.INVALID_SCOPE));
+                }
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
         return OAuth2TokenValidatorResult.failure(new OAuth2Error(OAuth2ErrorCodes.INVALID_SCOPE));
     }
-
 }
