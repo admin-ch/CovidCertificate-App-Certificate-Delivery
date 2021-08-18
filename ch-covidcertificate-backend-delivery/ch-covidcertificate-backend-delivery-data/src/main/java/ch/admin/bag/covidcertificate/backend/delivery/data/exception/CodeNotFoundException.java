a582fe6322caab6cda53bab5825eada432289f49
Patrick Amrein <amrein@ubique.ch>
2021-08-18 14:40:25 +0200

    make code variable final

--- ch-covidcertificate-backend-delivery/ch-covidcertificate-backend-delivery-data/src/main/java/ch/admin/bag/covidcertificate/backend/delivery/data/exception/CodeNotFoundException.java
+++ ch-covidcertificate-backend-delivery/ch-covidcertificate-backend-delivery-data/src/main/java/ch/admin/bag/covidcertificate/backend/delivery/data/exception/CodeNotFoundException.java
@@ -1,7 +1,7 @@
 package ch.admin.bag.covidcertificate.backend.delivery.data.exception;
 
 public class CodeNotFoundException extends Exception {
-    private String code;
+    private final String code;
 
     public CodeNotFoundException(String code) {
         this.code = code;
